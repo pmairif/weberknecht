cfc7312c85cad27c4658d43b0758ae39e60e6be8
Patrick Mairif <pmairif@highbyte-le.de>
2013-01-20 21:35:24 +0100

    AreaPath

---
+++ src-test/de/highbyte_le/weberknecht/request/routing/AreaPathTest.java
@@ -0,0 +1,63 @@
+/*
+ * AreaPathTest.java (weberknecht)
+ *
+ * Copyright 2013 Patrick Mairif.
+ * The program is distributed under the terms of the Apache License (ALv2).
+ *
+ * created: 20.01.2013
+ * tabstop=4, charset=UTF-8
+ */
+package de.highbyte_le.weberknecht.request.routing;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Vector;
+
+import org.junit.Test;
+
+/**
+ * testing AreaPath
+ *
+ * @author rick
+ */
+public class AreaPathTest {
+
+	@Test
+	public void testEmptyPathIgnored1() {
+		assertEquals(new AreaPath(), new AreaPath(""));
+	}
+
+	@Test
+	public void testEmptyPathIgnored2() {
+		AreaPath path = new AreaPath();
+		
+		path.addPath("");
+		assertEquals(0, path.getAreas().size());
+
+		path.addPath(null);
+		assertEquals(0, path.getAreas().size());
+	}
+	
+	@Test
+	public void testAddPath() {
+		AreaPath path = new AreaPath();
+
+		path.addPath("foo").addPath("bar");
+		
+		List<String> expected = new Vector<String>();
+		expected.add("foo");
+		expected.add("bar");
+		assertEquals(expected, path.getAreas());
+	}
+
+	@Test
+	public void testConstructor() {
+		AreaPath path = new AreaPath("a1", "a2");
+		
+		List<String> expected = new Vector<String>();
+		expected.add("a1");
+		expected.add("a2");
+		assertEquals(expected, path.getAreas());
+	}
+}
---
+++ src/de/highbyte_le/weberknecht/request/routing/AreaPath.java
@@ -0,0 +1,78 @@
+/*
+ * AreaPath.java (weberknecht)
+ *
+ * Copyright 2013 Patrick Mairif.
+ * The program is distributed under the terms of the Apache License (ALv2).
+ *
+ * tabstop=4, charset=UTF-8
+ */
+package de.highbyte_le.weberknecht.request.routing;
+
+import java.util.List;
+import java.util.Vector;
+
+/**
+ * path of areas to access the action
+ *
+ * @author rick
+ */
+public class AreaPath {
+
+	private final List<String> areas;
+	
+	public AreaPath() {
+		this.areas = new Vector<String>();
+	}
+
+	public AreaPath(String...areas) {
+		this.areas = new Vector<String>(areas.length);
+		for (String area: areas) {
+			addPath(area);
+		}
+	}
+	
+	public AreaPath addPath(String area) {
+		if (area != null && area.length() > 0)
+			this.areas.add(area);
+		
+		return this;
+	}
+	
+	/**
+	 * @return the areas
+	 */
+	public List<String> getAreas() {
+		return areas;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((areas == null) ? 0 : areas.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		AreaPath other = (AreaPath) obj;
+		if (areas == null) {
+			if (other.areas != null)
+				return false;
+		}
+		else if (!areas.equals(other.areas))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "AreaPath [areas=" + areas + "]";
+	}
+}
